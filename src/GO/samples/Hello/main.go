3249964e157a87b8b093a9267b375dfaf153f6a1
Ramon Castillo Sanchez <ramon.castillosanchez@rewe-group.com>
2026-01-28 16:36:01 +0100

    Inital commit after set up initial skeleton

---
+++ src/GO/samples/Hello/main.go
@@ -0,0 +1,28 @@
+// Package Declaration
+package main
+
+/*
+System: Tools
+Program Name: Sample
+Developer: Ramon Castillo
+			ramon.castillosanchez@rewe-group.com
+			ramon@rcastillo.net
+Version Date:January 27 2026
+Version :0.1
+Description:
+Usage:
+Changes:
+*/
+//
+//Continue with Imports
+//
+import "fmt"
+
+//
+//Define variable with global scope
+//
+// Functions
+//
+func main() {
+	fmt.Println("Hola, Mundo!")
+}
