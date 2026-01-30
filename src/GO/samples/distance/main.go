4f5e387d07a863b0e360185fda624e5dedae26ec
Ramon Castillo <ramon@rcastillo.net>
2026-01-30 20:43:53 +0100

    Distance version STDIN line by line

--- src/GO/samples/distance/main.go
+++ src/GO/samples/distance/main.go
@@ -7,12 +7,13 @@ import (
 
 func main() {
     fmt.Println("input text:")
-    var n1, n2 float32
+    var n1, n2 , r_distance float32
     n, err := fmt.Scanln(&n1, &n2)
     if err != nil {
         log.Fatal(err)
     }
-
+    r_distance = n2/n1*100-100
     fmt.Printf("number of items read: %d\n", n)
     fmt.Printf("read line: %s %s\n", n1, n2)
+    fmt.Printf("result: %.2f%%\n",r_distance)
 }
